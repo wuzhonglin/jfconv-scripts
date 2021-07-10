0c47628546b0737123f98171fef53ee6de8a90da
Hiroshi Manabe <manabe.hiroshi@gmail.com>
2021-07-11 01:54:09 +0900

    use PYTHONUNBEFFERED.

--- fanjian.sh
+++ fanjian.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 cd `dirname $0`
+export PYTHONUNBUFFERED=1
 ./jianfan_preprocess.pl --reverse | ./decoder.py ./jianti.bin | ./jianfan_postprocess.pl
--- jianfan.sh
+++ jianfan.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 cd `dirname $0`
+export PYTHONUNBUFFERED=1
 ./jianfan_preprocess.pl | ./decoder.py ./fanti.bin | ./jianfan_postprocess.pl
